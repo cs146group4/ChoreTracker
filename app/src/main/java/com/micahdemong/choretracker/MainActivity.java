b96a605b3f42d849e35607a33a12c080fbbab765
mdemong <mdemong1@gmail.com>
2016-05-05 19:28:34 -0700

    Created SettingsActivity.java and moved SettingsFragment there

--- app/src/main/java/com/micahdemong/choretracker/MainActivity.java
+++ app/src/main/java/com/micahdemong/choretracker/MainActivity.java
@@ -119,13 +119,5 @@ public class MainActivity extends AppCompatActivity
         return true;
     }
 
-    public static class SettingsFragment extends PreferenceFragment {
-        @Override
-        public void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
 
-            // Load the preferences from an XML resource
-            addPreferencesFromResource(R.xml.preferences);
-        }
-    }
 }
